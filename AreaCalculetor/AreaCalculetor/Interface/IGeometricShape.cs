9d2968a6ffc4e51a0555de7e17cba4728dc107c2
Edit4 <109595953+Edit4@users.noreply.github.com>
2022-08-01 14:48:35 +0500

    Add files via upload

---
+++ AreaCalculetor/AreaCalculetor/Interface/IGeometricShape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AreaCalculetor.Interface
+{
+    /// <summary>
+    /// Интерфейс для геометрических фигур
+    /// </summary
+    interface IGeometricShape
+    {
+        /// <summary>
+        /// Вычисление площади геометрической фигуры
+        /// </summary>
+        /// <returns>Площадь в квадратичных единицах</returns>
+        public double GetArea();
+    }
+}
